9f34eba4c73b3de31db66c1a4a76afe7cc91378a
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-11-26 18:04:58 -0800

    working commit - Created using LeetHub

---
+++ valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+      HashMap<Character, Character> map = new HashMap<Character, Character>();
+      map.put(']', '[');
+      map.put('}', '{');
+      map.put(')', '(');
+      Stack<Character> stack = new Stack<Character>();
+      for ( char c : s.toCharArray())
+      {
+          if (map.containsKey(c) && !stack.isEmpty() )
+          {
+              
+              if(map.get(c) != stack.pop() )
+                  return false;
+          }
+          else
+           stack.push(c);
+      }
+        return stack.isEmpty();
+    }
+}

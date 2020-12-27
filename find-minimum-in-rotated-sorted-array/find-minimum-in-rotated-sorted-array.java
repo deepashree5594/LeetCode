b28d9cb322f122815c28cea749ea3a483270981c
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-12-27 00:09:07 -0800

    Time: 0 ms (100.00%), Space: 38.7 MB (18.10%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,50 +1,59 @@
-// Bruteforce approach - O(n)
-//class Solution {
-//     public int findMin(int[] nums) {
-//         int min = nums[0];
-//         for ( int i = 1; i < nums.length; i++)
-//         {
-//             min = Math.min(min, nums[i]);
-//         }
-//         return min;
-//     }
-// }
+// Time Complexity : Binary Search - O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : YES
 ​
-​
-​
-// Binary Search way - O(logn)
 class Solution {
     public int findMin(int[] nums) {
-      int min = Integer.MAX_VALUE;
+     if (nums[0] < nums[nums.length-1]) return nums[0];
       int low = 0;
-      int high = nums.length-1 ;
-      while (low <= high)
+      int high = nums.length-1;
+       
+        
+      while ( low <= high)
       {
+          if (nums[low] < nums[high]) return nums[low];
+          
           int mid = low + (high-low)/2;
-          min = Math.min(min, nums[mid]);
-          if(nums[low] <= nums[mid])
+          
+          if ((mid == 0 || nums[mid] < nums[mid-1])
+              && (mid == nums.length -1 || nums[mid] < nums[mid+1] ))
+              return nums[mid];
+          
+          else if (nums[low] <= nums[mid])
           {
-              if(nums[low] <= nums[high] && nums[mid] <= nums[high])
-              {
-                  high = mid - 1;
-              }
-              else
-              {
-                  low = mid + 1;
-              }
+              low = mid + 1;              
           }
           else
           {
-              if (nums[low] >= nums[high] && nums[mid] <= nums[high])
-              {
-                  high = mid - 1;
-              }
-              else
-              {
-                  low = mid + 1 ;
-              }
+              high = mid - 1;
           }
       }
-      return min;
+      return -1;
+        
     }
+    
 }
+​
+// class Solution {
+//     public int findMin(int[] nums) {
+//       int min = Integer.MAX_VALUE;
+//       int low = 0;
+//       int high = nums.length-1 ;
+//       while (low <= high)
+//       {
+//           int mid = low + (high-low)/2;
+//           min = Math.min(min, nums[mid]);
+//           if(nums[low] <= nums[mid])
+//           {
+//               if(nums[low] <= nums[high] && nums[mid] <= nums[high])
+//               {
+//                   high = mid - 1;
+//               }
+//               else
+//               {
+//                   low = mid + 1;
+//               }
+//           }
+//           else
+//           {
+//               if (nums[low] >= nums[high] && nums[mid] <= nums[high])

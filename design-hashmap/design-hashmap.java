3083fda9d6d26d6d191562b5d8d07c4947cae538
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-12-26 22:52:40 -0800

    Time: 12 ms (85.73%), Space: 44.2 MB (53.37%) - LeetHub

--- design-hashmap/design-hashmap.java
+++ design-hashmap/design-hashmap.java
@@ -1,36 +1,59 @@
-class MyHashMap {
     
-    /** Initialize your data structure here. */
-    int[] arr;
-    
-    public MyHashMap() {
-        arr = new int[1000000];
-        Arrays.fill(arr, -1);
+    private Node find(int index, int key)
+    {
+        int i = getIndex(key);
+        if ( nodes[i] == null)
+            return nodes[index] = new Node(-1,-1);
+        
+        Node curr = nodes[i];
+        Node prev = null;
+        while ( curr != null && curr.key != key)
+        {
+             prev = curr;
+             curr = curr.next;
+        }
+        return prev;
     }
-    
-    
     /** value will always be non-negative. */
     public void put(int key, int value) {
-        arr[key] = value;
+       int index = getIndex(key);
+       if (nodes[index] == null) 
+       {
+           Node node = new Node(-1,-1);
+       }
+       Node prev = find(index, key);
+       if (prev.next == null)
+       {
+           prev.next = new Node(key, value);
+       }
+       else
+       {
+         prev.next.value = value;
+       }
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        if (arr[key] >= 0)
-            return arr[key];
-        return -1;
+       int index = getIndex(key);
+       if (nodes[index] == null) 
+           return -1;
+      Node prev = find(index, key);
+       if (prev.next == null)
+            return -1;
+       return prev.next.value;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        arr[key] = -1;
+       int index = getIndex(key);
+        if (nodes[index] == null) 
+            return;
+        Node prev = find(index, key);
+        if (prev.next == null)
+            return;
+        else
+            prev.next = prev.next.next;
+        
     }
 }
 ​
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */

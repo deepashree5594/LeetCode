d912afa5847b2e34e26d10d9b66a097db8d343ac
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-12-26 23:35:18 -0800

    Time: 0 ms (100.00%), Space: 42.1 MB (75.81%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,68 @@
+// Time Complexity : Binary Search - O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : YES
 ​
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-         int first = -1 ;
-         int last = -1;
-         for ( int i = 0; i < nums.length; i++){
-             if (nums[i] == target)
-             {
-                 first = i;
-                 break;
-             } 
-         }
-        for ( int i = nums.length-1; i >= 0; i--) {
-             if (nums[i] == target)
-             {
-                 last = i;
-                 break;
-             }             
-         }
-        return new int[]{first,last};
-    }
-}
+        if ( nums == null || nums.length == 0) return new int[]{-1, -1};
+  
+        int first = binarySearchFirst(nums, target);
+        int second = binarySearchSecond(nums, target);
+        return new int[]{first, second};
+      }
 ​
+      private int binarySearchFirst(int[] nums, int target)
+      {
+          int low = 0;
+          int high = nums.length-1;
+          while ( low <= high)
+          {
+              int mid = low + (high-low)/2;
+              if (nums[mid] == target)
+              {
+                  if (mid == 0 || nums[mid] > nums[mid-1])
+                      return mid;
+                  else
+                      high = mid -1;
+              }
+              else if (nums[mid] > target)
+              {
+                  high = mid - 1;
+              }
+              else
+              {
+                  low = mid + 1;
+              }
+          }
+         return -1;
+      }
+    
+      private int binarySearchSecond(int[] nums, int target)
+      {
+          int low = 0;
+          int high = nums.length-1;
+          while ( low <= high)
+          {
+              int mid = low + (high-low)/2;
+              if (nums[mid] == target)
+              {
+                  if (mid == nums.length-1 || nums[mid+1] > nums[mid])
+                      return mid;
+                  else
+                      low = mid + 1;
+              }
+              else if (nums[mid] > target)
+              {
+                  high = mid - 1;
+              }
+              else
+              {
+                  low = mid + 1;
+              }
+          }
+         return -1;
+      }
+}
+// Time Complexity : Linear Search - O(2N) - O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : YES

c83af19244020cf52486e0d3e73b1fc5dd9236f7
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-12-28 19:45:23 -0800

    Time: 12 ms (28.93%), Space: 39.3 MB (37.35%) - LeetHub

--- isomorphic-strings/isomorphic-strings.java
+++ isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,56 @@
+// Time Complexity: O(N) - length of s and t strings
+// Space Complexity: O(K) - K is constant (128)
+// Did it run sucessfully on Leetcode? Yes
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
+//     public boolean isIsomorphic(String s, String t) {
 ​
-      if (s.length() != t.length())
-            return false;
+      
         
-     int[] arr1 = new int[128];
-     int[] arr2 = new int[128];
+//      int[] arr1 = new int[128];
+//      int[] arr2 = new int[128];
         
-     for (int i = 0; i < s.length(); i++)
-     {
-         if (arr1[s.charAt(i)] != arr2[t.charAt(i)] )
-             return false;
-         arr1[s.charAt(i)] = i+1;
-         arr2[t.charAt(i)] = i+1;
-     }
-        return true;
+//      for (int i = 0; i < s.length(); i++)
+//      {
+//          if (arr1[s.charAt(i) ] != arr2[t.charAt(i) ] )
+//              return false;
+//          arr1[s.charAt(i) ] = i+1;
+//          arr2[t.charAt(i) ] = i+1;
+//      }
+//         return true;
     
+//     }
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Character> sMap = new HashMap<Character, Character>();
+        HashMap<Character, Character> tMap = new HashMap<Character, Character>();
+        int i = 0; 
+        int j = 0;
+        while ( i < s.length() && j < t.length())
+        {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(j);
+            if (sMap.containsKey(sChar))
+            {
+                if (sMap.get(sChar) != tChar)
+                    return false;
+            }
+            else
+            {
+                sMap.put(sChar, tChar);
+            }
+            if (tMap.containsKey(tChar))
+            {
+                if (tMap.get(tChar) != sChar)
+                    return false;
+            }
+            else
+            {
+                tMap.put(tChar, sChar);
+            }
+            i++;
+            j++;
+        }
+        return true;
     }
 }

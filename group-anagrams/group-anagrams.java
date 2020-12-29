fb077242fd5d1f3616c0e1793686f011c1ca7f83
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2020-12-28 23:02:13 -0800

    Time: 5 ms (98.77%), Space: 42.2 MB (61.25%) - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,37 +1,49 @@
-//O(NK)
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        // List<List<String>> list = new ArrayList();
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-        int[] charCountArr;
-        StringBuilder sb;
-        for ( String str : strs)
-        {     
-            charCountArr = new int[26];
+//             if  (!map.containsKey(s))        
+//             {
+//                 map.put(s, new ArrayList());
+//             }
+//             map.get(s).add(str);
             
-            for ( int i = 0; i < str.length(); i++)
-            {               
-                charCountArr[str.charAt(i) - 'a']++;
-            }
-            sb = new StringBuilder();
-            for ( int i = 0; i < 26; i++)
-            {
-                sb.append(charCountArr[i]).append('#');
-            }
-            String s = sb.toString();
-            if  (!map.containsKey(s))        
-            {
-                map.put(s, new ArrayList());
-            }
-            map.get(s).add(str);
-            
-        }
-        return new ArrayList(map.values());
+//         }
+//         return new ArrayList(map.values());
         
+//     }
+// }
+​
+​
+//TC: O(NK)
+//SC: O(NK) - N is no. of the sorted strings (key) and K is the avg length of arraylist which holds the answer ( groups anagrams)
+class Solution {
+     public List<List<String>> groupAnagrams(String[] strs) {
+         if ( strs.length == 0) 
+           return new ArrayList();
+         Map<Long, List> map = new HashMap<Long, List>();
+         for ( String s : strs)
+         {
+             long primeNumber = getPrimeFactor(s);
+             if (!map.containsKey(primeNumber))
+             {
+                 map.put(primeNumber, new ArrayList());
+             }
+             map.get(primeNumber).add(s);
+         }
+         return new ArrayList(map.values());         
+     }
+    private long getPrimeFactor(String s)
+    {
+        int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,93,97,101,103};
+        long result = 1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            result *= s.charAt(i) * primes[s.charAt(i) - 'a'];
+        }
+        return result;
     }
 }
 ​
-// O(NKlogK)
+​
+//TC: O(NKlogK)
+//SC: O(NK) - N is no. of the sorted strings (key) and K is the avg length of arraylist which holds the answer ( groups anagrams)
 // class Solution {
 //      public List<List<String>> groupAnagrams(String[] strs) {
 //          if ( strs.length == 0) return new ArrayList();
@@ -41,12 +53,3 @@ class Solution {
 //              char[] charArray = s.toCharArray();
 //              Arrays.sort(charArray);
 //              String key = String.valueOf(charArray);
-//              if (!map.containsKey(key))
-//              {
-//                  map.put(key, new ArrayList());
-//              }
-//              map.get(key).add(s);
-//          }
-//          return new ArrayList(map.values());         
-//      }
-// }

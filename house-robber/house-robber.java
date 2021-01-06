cbff9964e241b68b8e783050fbd2f88d2fff1953
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-01-06 11:35:46 -0800

    Time: 0 ms (100.00%), Space: 36.1 MB (95.02%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,17 +1,47 @@
+//TC: O(N) - n is the no. of elements in nums array
+//SC: O(N)
+// Did it run successfully on LeetCode? : 
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
-        if ( nums.length == 1) 
-            return nums[0];
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
-        int[] dp = new int[nums.length+1];
-        dp[1] = nums[0];
-        for (int i = 2; i < dp.length; i++)
-        {
-            dp[i] = Math.max(dp[i-2] + nums[i-1], dp[i-1]);
-        }
-        return dp[dp.length-1];
+       if (nums == null || nums.length == 0)
+             return 0;
+       int dp[][] = new int[nums.length+1][2];
+       dp[0][0] = 0;
+       dp[0][1] = 0;
+       for ( int i = 1; i < dp.length; i++)
+       {
+               dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+               dp[i][1] = dp[i-1][0] + nums[i-1];          
+       }
+        return Math.max(dp[dp.length-1][0],dp[dp.length-1][1]);
     }
+    
 }
+​
+​
+​
+// Recursion
+//TC: O(2^N) - n is the no. of elements in nums array
+//SC: O(1)
+// Did it run successfully on LeetCode? : TLE
+// class Solution {
+//     public int rob(int[] nums) {
+//        if (nums == null || nums.length == 0)
+//              return 0;
+//         return helper(nums, 0, 0);
+//     }
+//     private int helper(int[] nums, int index, int amount)
+//     {
+//         //base case
+//         if ( index >= nums.length)
+//             return amount;     
+//         //logic
+        
+//         //choose
+//         int case1 = helper(nums, index+2, amount + nums[index]);
+//         // dont choose
+//         int case2 = helper(nums, index+1, amount);
+//         return Math.max(case1, case2);
+//     }
+// }
+​

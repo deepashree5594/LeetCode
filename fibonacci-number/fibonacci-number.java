31726e7555a43dbf7789614a10a02ed67b09f70d
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-01-11 13:39:08 -0800

    Time: 0 ms (100.00%), Space: 35.8 MB (53.18%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -6,23 +6,24 @@
 //     }
 // }
 ​
-class Solution{
-    public int fib(int N)
-    {
-        if (N == 0) 
-            return 0;
-        if (N == 1)
-            return 1;
-        int[] dp = new int[N];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i < dp.length; i++)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[dp.length-1];
-    }
-}
+// 1D DP solution
+// class Solution{
+//     public int fib(int N)
+//     {
+//         if (N == 0) 
+//             return 0;
+//         if (N == 1)
+//             return 1;
+//         int[] dp = new int[N];
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for (int i = 2; i < dp.length; i++)
+//         {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[dp.length-1];
+//     }
+// }
 ​
 // class Solution {
 //     public int fib(int N) {
@@ -32,23 +33,23 @@ class Solution{
 // }
 ​
 ​
-// class Solution {
-//     public int fib(int N) {
-//        if (N <=1 )
-//           return N;
-//         if (N == 2)
-//           return 1;
-//         int current = 0;
-//         int prev1 = 1;
-//         int prev2 = 1;
-//         for ( int i = 3; i <= N ; i++)
-//         {
-//             current = prev1 + prev2;
-//             prev2 = prev1;
-//             prev1 = current;
-//         }
-//         return current;
-//     }
-// }
+class Solution {
+    public int fib(int N) {
+       if (N > 0 && N<=2 )
+          return 1;
+        // if (N == 2)
+        //   return 1;
+        int current = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+        for ( int i = 3; i <= N ; i++)
+        {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+}
 ​
 ​

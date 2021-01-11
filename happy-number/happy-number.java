5389ec47a186ebf18c52fb58d104f91148c2ab67
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-01-10 19:56:36 -0800

    Time: 2 ms (20.09%), Space: 38.2 MB (5.24%) - LeetHub

--- happy-number/happy-number.java
+++ happy-number/happy-number.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-       HashSet<Integer> seen = new HashSet<>();
-       while (n != 1){
+        HashSet<Integer> seen = new HashSet<>();
+       while (n != 1)
+        {
             int sum = 0;
-            int current = n;
-            while (current != 0)
-            {                
-                sum = sum + (int)Math.pow(current % 10, 2);
-                current = current / 10;                
-            }
-           if (seen.contains(sum))
-               return false;
-                
-            seen.add(sum);
-            n = sum;
-       }
+            int temp = n;
+             while ( temp != 0 )
+             {
+                 int digit = temp % 10;
+                 sum += digit * digit;
+                 temp = temp / 10;               
+             }           
+             n = sum;
+             if (seen.contains(sum))
+                 return false;
+             seen.add(sum);
+        }
         return true;
     }
 }

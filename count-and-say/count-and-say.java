495edfbf8def2a4e06659c69921d4bde43c6ef6e
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-01-17 18:31:53 -0800

    Time: 13 ms (32.33%), Space: 38.4 MB (46.37%) - LeetHub

---
+++ count-and-say/count-and-say.java
@@ -0,0 +1,24 @@
+// https://www.youtube.com/watch?v=-wB1xj-kOe0
+class Solution {
+    public String countAndSay(int n) {
+        if ( n <= 0 ) return "";
+        String result = "1";
+        while ( n > 1) 
+        {
+            StringBuilder sb = new StringBuilder();
+            for ( int i = 0; i < result.length(); i++)
+            {
+                int count = 1;
+                while (i+1 < result.length() && result.charAt(i) == result.charAt(i+1))
+                {
+                    count++;
+                    i++;
+                }
+                sb.append(count).append(result.charAt(i));
+            }
+            result = sb.toString();
+            --n;
+        }
+        return result;
+    }
+}

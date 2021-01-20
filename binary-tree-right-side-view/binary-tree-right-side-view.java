9a046ce900c89cf7827a57ae4bac8a00cdc6bab7
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-01-19 19:27:38 -0800

    Time: 0 ms (100.00%), Space: 37.8 MB (43.38%) - LeetHub

---
+++ binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     List<Integer> result;
+//     public List<Integer> rightSideView(TreeNode root) {
+//         result = new ArrayList();
+//         if ( root == null)
+//         return result;
+//        dfs(root, 0);
+//        return result;      
+//     }
+//     private void dfs(TreeNode root, int level)
+//     {
+//         //base
+//         if ( root == null)
+//           return;
+//         //
+//         if (level == result.size())
+//            result.add(root.val);
+//         dfs(root.right, level+1);
+//         //stack.pop()
+//         dfs(root.left, level+1);
+//     }
+// }
+​
+class Solution {
+    List<Integer> result;
+    public List<Integer> rightSideView(TreeNode root) {
+        result = new ArrayList();
+        if ( root == null)
+        return result;
+       dfs(root, 0);
+       return result;      
+    }
+    private void dfs(TreeNode root, int level)
+    {
+        //base
+        if ( root == null)
+          return;
+        //
+        if (level == result.size())
+           result.add(root.val);
+        else
+            result.set(level, root.val);
+        dfs(root.left, level+1);
+        //stack.pop()
+        dfs(root.right, level+1);
+    }
+}

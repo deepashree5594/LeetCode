b168200e39e913629b00a904aa9fde60232fc64d
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-02-07 13:26:20 -0800

    Time: 0 ms (100.00%), Space: 38.5 MB (36.94%) - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,41 +1,46 @@
-// https://www.youtube.com/watch?v=-3Lt-EwR_Hw&ab_channel=GauravSen
 //TC: O(logN)
+//SC: O(1)
 class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-        if ( n < 0)
-        {
-            x = 1/x;
-            N = -N;
-        }
-        double ans = 1;
-        double currentProduct = x;
-        for ( long i = N; i  > 0; i = i/2)
-        {
-            if ((i%2) == 1)
-            {
-                ans = ans * currentProduct;
-            }
-            currentProduct = currentProduct * currentProduct;
-        }
-   return ans;   
-    }
+    public double myPow(double x, int n) {
+       //base
+        if ( n == 0) 
+            return 1;
+       //logic
+        double temp = myPow(x, n/2);
+        //stack.pop() happens
+        if ( n % 2 == 0)      //n is even 
+        {
+            return temp * temp;
+        }
+        else //n is odd 
+        {
+            if ( n < 0 ) // if n is negative,  multiply it by 1/x
+            {
+                return temp * temp * (1/x);
+            }
+            else // if n is positive, just multiply it by x
+            {
+                return temp * temp * x;
+            }
+        }
+    }
 }
-​
+
 // TC: O(N)
+//SC: O(1)
 // class Solution {
-//     public double myPow(double x, int n) {
-//         long N = n;
-//         if ( n < 0)
-//         {
-//             x = 1/x;
-//             N = -N;
-//         }
-//         double ans = 1;
-//         for ( int i = 1; i <= N; i++)
-//         {          
-//               ans = ans * x;         
-//         }
-//         return ans;
-//     }
+//     public double myPow(double x, int n) {
+//         long N = n;
+//         if ( n < 0)
+//         {
+//             x = 1/x;
+//             N = -N;
+//         }
+//         double ans = 1;
+//         for ( int i = 1; i <= N; i++)
+//         {          
+//               ans = ans * x;         
+//         }
+//         return ans;
+//     }
 // }

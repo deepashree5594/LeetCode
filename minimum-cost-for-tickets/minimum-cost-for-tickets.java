547a61c97352531bf272469a9d3af5935328f20b
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-03 13:44:05 -0800

    Time: 1 ms (79.26%), Space: 38.3 MB (36.04%) - LeetHub

---
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        if (days == null || days.length == 0)
+            return 0;
+        HashSet<Integer> daysSet = new HashSet();
+        for (int day: days)
+        {
+            daysSet.add(day);
+        }
+        int maxDay = days[days.length-1];
+        int[] dp = new int[maxDay+1];
+        dp[0] = 0;
+     
+        for ( int i = 1; i < dp.length; i++)
+        {
+            if (daysSet.contains(i)) 
+            {             
+               dp[i] = Math.min(dp[Math.max(i-1, 0)] + costs[0],
+                                Math.min(dp[Math.max(i-7, 0)] + costs[1],
+                                         dp[Math.max(i-30, 0)] + costs[2]));  
+            }
+            else
+            {
+                dp[i] = dp[i-1];
+            }
+        }
+        return dp[dp.length-1];
+    }
+}

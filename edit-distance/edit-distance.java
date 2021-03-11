7d5636ad08570e7da02b97306bf17adacb4421e2
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-10 23:24:24 -0800

    Time: 4 ms (91.82%), Space: 39.4 MB (34.00%) - LeetHub

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -1,3 +1,6 @@
+// TC: O(m*n) , m-> length of word2, n-> length of word1
+// SC: O(m*n) , m-> length of word2, n-> length of word1
+// Did it run successfully on Leetcode? : Yes
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word2.length();
@@ -19,14 +22,14 @@ class Solution {
         {
             for ( int j = 1; j <= n; j++)
             {
-                // if current chars are same, copy up diagonal value
+                // if current chars are same, copy up diagnal value
                 if (word1.charAt(j-1) == word2.charAt(i-1))
                 {                    
                      dp[i][j] = dp[i-1][j-1];
                 }
                 else // if current chars are NOT SAME,
                 {
-                    // 1 + min(delete -> one left, update -> up diagonal, add-> prev row val)
+                    // 1 + min(delete -> one left, update -> up diagnal, add-> prev row val)
                     dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
                 }
             }

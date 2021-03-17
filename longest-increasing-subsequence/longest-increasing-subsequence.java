737672a44f0744321c3f67b66cd8aa3e321c5858
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-16 18:24:25 -0700

    Time: 2 ms (99.07%), Space: 38.3 MB (95.96%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,77 @@
+// Binary Search 
+// TC: O(N^2)
+// SC: O(N)
+// Did ir run successfully on Leetcode? : Yes
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if ( nums == null || nums.length == 0)
             return 0;
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
+        int[] arr = new int[nums.length];
+        int len = 1;
+        arr[0] = nums[0];
         int result = 1;
-        for ( int i = 0; i < dp.length; i++)
+        for ( int i = 1; i < arr.length; i++ )
         {
-            for ( int j = 0 ; j < i; j++)
+            if (nums[i] > arr[len-1])
             {
-                if (nums[i] > nums[j] )
-                {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    result = Math.max(result, dp[i]);
-                }
+                arr[len] = nums[i];
+                len++;
+            }
+            else
+            {
+                int index = closestBinarySearch(arr, 0, len, nums[i]);
+                arr[index] = nums[i];
+            }
+           // result = Math.max(len, result);
+        }
+        return len;
+    }
+    private int closestBinarySearch(int[] arr, int low, int high, int target)
+    {
+        while ( low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if ( arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
             }
         }
-        return result;
+        return low;
     }
 }
+
+
+
+
+
+// DP 
+// TC: O(N^2)
+// SC: O(N)
+// Did ir run successfully on Leetcode? : Yes
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         if ( nums == null || nums.length == 0)
+//             return 0;
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, 1);
+//         int result = 1;
+//         for ( int i = 0; i < dp.length; i++)
+//         {
+//             for ( int j = 0 ; j < i; j++)
+//             {
+//                 if (nums[i] > nums[j] )
+//                 {
+//                     dp[i] = Math.max(dp[i], dp[j] + 1);
+//                     result = Math.max(result, dp[i]);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }

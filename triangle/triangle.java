43e4d159f256f2aff733c8e9c3b18beceb90c285
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-23 11:10:04 -0700

    Time: 1 ms (96.81%), Space: 38.7 MB (92.94%) - LeetHub

---
+++ triangle/triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0)
+            return 0;
+    
+        int result = 0;
+        int[] dp = new int[triangle.size()];
+        
+        //populating last row
+        for (int j = 0; j < dp.length; j++)
+            dp[j] = triangle.get(dp.length-1).get(j);
+        
+        // DP bottom up- start from second-last row
+        for ( int i = dp.length - 2; i >=0; i-- ) {
+            for ( int j = 0; j <= i; j++) {
+               dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+         }
+       }
+       return dp[0];
+    }
+}

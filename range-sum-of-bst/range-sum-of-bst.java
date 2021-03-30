7745823d3cd4d0e00400227e7553c3bf3ec048f3
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-30 14:14:52 -0700

    Time: 1 ms (50.06%), Space: 46.9 MB (69.57%) - LeetHub

---
+++ range-sum-of-bst/range-sum-of-bst.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return 0;
+         helper(root, low, high);
+        return result;
+    }
+    private void  helper(TreeNode root, int low, int high){
+        //base
+        if ( root == null){
+            return;
+        }
+        //logic
+        if (root.val >= low && root.val <= high)
+            result += root.val;
+        helper(root.left, low, high);
+        helper(root.right, low, high);
+    }
+}

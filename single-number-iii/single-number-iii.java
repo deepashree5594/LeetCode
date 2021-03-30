6e40106f3ee2eacc0c6340dd03dfe0d3e470d2ad
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-30 11:50:32 -0700

    Time: 1 ms (96.47%), Space: 38.9 MB (91.11%) - LeetHub

---
+++ single-number-iii/single-number-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int bitmask = 0;
+        for (int num : nums){
+            bitmask = bitmask ^ num;
+        }
+        int diff = bitmask & (-bitmask);
+        int bitmask2 = 0;
+        for (int num : nums){
+            if ((diff & num) != 0){
+                bitmask2 = bitmask2 ^ num;
+            }
+        }
+        return new int[]{bitmask2, bitmask2^bitmask};
+    }
+}

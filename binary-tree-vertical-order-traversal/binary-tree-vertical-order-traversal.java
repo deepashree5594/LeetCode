fced3f341076340343c10bd1a00a36bfa0f914a4
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-03-31 11:43:53 -0700

    Time: 1 ms (100.00%), Space: 38.9 MB (95.03%) - LeetHub

---
+++ binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution{
+    class Pair{
+      TreeNode node;
+      int column;
+      Pair(TreeNode node, int column ){
+          this.node = node;
+          this.column = column;
+      }
+    }
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        if (root == null)
+          return new ArrayList();
+        List<List<Integer>> result = new ArrayList();
+        HashMap<Integer, List<Integer>> map = new HashMap();
+        int min = 0;
+        int max = 0;
+        Queue<Pair> q = new LinkedList();
+        q.offer(new Pair(root, 0));
+        while (!q.isEmpty()){
+            Pair p = q.poll();
+            TreeNode currNode = p.node;
+            int currColumn = p.column;
+            if (!map.containsKey(currColumn)){
+                map.put(currColumn, new ArrayList());
+            }
+            map.get(currColumn).add(currNode.val);
+            if (currNode.left != null){
+                q.add(new Pair(currNode.left, currColumn - 1 ));
+                min = Math.min(min, currColumn - 1 );
+            }
+            if (currNode.right != null){
+                q.add(new Pair(currNode.right, currColumn + 1 ));
+                max = Math.max(max, currColumn + 1 );
+            }
+        }
+        for (int i = min; i <= max; i++){
+            result.add(map.get(i));
+        }
+        return result;
+    }
+}

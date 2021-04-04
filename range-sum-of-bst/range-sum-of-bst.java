76d7ad2b6fe96bdaee674ce67dfbfa2a0c7818f2
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-04 12:46:28 -0700

    Time: 1 ms (48.82%), Space: 46.3 MB (98.83%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.java
+++ range-sum-of-bst/range-sum-of-bst.java
@@ -13,23 +13,56 @@
  *     }
  * }
  */
+//TC: O(N)
+//SC: O(N)
+//Dit it run successfully on Leetcode?:Yes
+// class Solution {
+//     int result;
+//     public int rangeSumBST(TreeNode root, int low, int high) {
+//         if (root == null)
+//             return 0;
+//          helper(root, low, high);
+//         return result;
+//     }
+//     private void  helper(TreeNode root, int low, int high){
+//         //base
+//         if ( root == null){
+//             return;
+//         }
+//         //logic
+//         if (root.val >= low && root.val <= high)
+//             result += root.val;
+//         //if currnet node's value is greater than low only then traverse the left subtree since its a BST left substree will have values less than currnet node
+//         if (root.val > low){
+//              helper(root.left, low, high);
+//         }
+//         //if currnet node's value is less than high only then traverse the right subtree since its a BST right substree will have values grater than currnet node
+//         if (root.val < high){
+//              helper(root.right, low, high);
+//         }  
+//     }
+// }
+
+
 class Solution {
-    int result;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null)
             return 0;
-         helper(root, low, high);
-        return result;
-    }
-    private void  helper(TreeNode root, int low, int high){
-        //base
-        if ( root == null){
-            return;
+     Queue<TreeNode> q = new LinkedList();
+     q.offer(root);
+     int result = 0;
+        while (!q.isEmpty()){
+            TreeNode curr = q.poll();
+            if (curr.val >= low && curr.val <= high){
+                result = result + curr.val;
+            }
+            if (curr.val > low && curr.left != null){
+                q.offer(curr.left);
+            }
+            if (curr.val < high && curr.right != null){
+                q.offer(curr.right);
+            }    
         }
-        //logic
-        if (root.val >= low && root.val <= high)
-            result += root.val;
-        helper(root.left, low, high);
-        helper(root.right, low, high);
+      return result;
     }
 }

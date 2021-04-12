889bd2f7fc71c504d04226f14d017f413203be9b
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-12 15:58:53 -0700

    Time: 1 ms (100.00%), Space: 38.6 MB (97.45%) - LeetHub

---
+++ string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,29 @@
+// O(N) solution
+class Solution {
+    public int myAtoi(String str) {
+        if ( str.length() == 0 ) return 0;
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        while ( i < str.length() && str.charAt(i) == ' ')
+        {
+            i++;
+        }
+        if ( i < str.length() && (str.charAt(i) == '+' || str.charAt(i) == '-'))
+        {
+            sign = (str.charAt(i) == '+') ? 1: -1;
+            i++;
+        }
+        while ( i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9' )
+        {
+              if (result > Integer.MAX_VALUE/10 || 
+                  (result == Integer.MAX_VALUE/10 && str.charAt(i) - '0'> Integer.MAX_VALUE % 10))
+              {
+                  return (sign == 1)? Integer.MAX_VALUE:Integer.MIN_VALUE;
+              }
+              result = result * 10 + (str.charAt(i) - '0');
+              i++;
+        }
+        return result * sign;
+    }
+}

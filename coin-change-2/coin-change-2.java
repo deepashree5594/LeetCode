a7944b04eeb0f532d7cd84917d835c49d7d21bdb
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-18 16:45:41 -0700

    Time: 16 ms (14.88%), Space: 47.6 MB (6.35%) - LeetHub

--- coin-change-2/coin-change-2.java
+++ coin-change-2/coin-change-2.java
@@ -1,46 +1,40 @@
-// TC: O(2^N) -> Recursive solution 
-// SC: O(N) -> recursive stack space
-// Did it run successfully on Leetcode? : TLE
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for ( int i = 0; i < dp.length; i++ ){
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < dp.length;i++){
+            for (int j = 1; j < dp[0].length; j++){
+                if (j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
+
 // class Solution {
 //     public int change(int amount, int[] coins) {
-//         return helper(coins, 0, amount);
+//         if (coins == null || coins.length == 0)
+//             return 0;
+//         return helper(coins, 0, amount, noOfWays);
 //     }
 //     private int helper(int[] coins, int index, int amount){
 //         //base
+//         if (index >= coins.length && amount != 0)
+//             return -1;
 //         if (amount == 0){
-//             return 1;
+//             return 
 //         }
-//         if (amount < 0 || index >= coins.length){
-//             return 0;
-//         }  
 //         //logic
-//         //dont choose
-//         int case1 = helper(coins, index, amount-coins[index]);
-//         //choose
+//         //case1 - choose
+//         int case1 = helper(coins, index+1, amount - coins[index]);
+//         //case2 - don't choose
 //         int case2 = helper(coins, index+1, amount);
 //         return case1+case2;
 //     }
 // }
-
-// Bottom up DP 
-// TC: O(M*N) -> O(coins.length + 1 * amount +1 ) 
-// SC: O(M*N) -> dp matrix size
-// Did it run successfully on Leetcode? : Yes
-class Solution {
-    public int change(int amount, int[] coins) {
-       int[][] dp = new int[coins.length+1][amount+1];
-        for (int i = 0; i < dp.length; i++){
-            dp[i][0] = 1;
-        }
-         for (int i = 1; i < dp.length; i++){
-             for (int j = 1; j < dp[0].length; j++){
-                 if (j < coins[i-1]){
-                     dp[i][j] = dp[i-1][j];
-                 }else{
-                     dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
-                 }
-             }
-         }
-        return dp[dp.length-1][dp[0].length-1];
-    }
-}

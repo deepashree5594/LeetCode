636d82b36d354a404e93de7b3a613c9b70faab61
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-18 13:31:23 -0700

    Time: 3 ms (46.25%), Space: 40.3 MB (5.39%) - LeetHub

--- delete-and-earn/delete-and-earn.java
+++ delete-and-earn/delete-and-earn.java
@@ -1,27 +1,21 @@
 class Solution {
-    public int deleteAndEarn(int[] nums) {
-       if ( nums == null || nums.length == 0)
-           return 0;
-       int max = 0;
-       for (int n : nums)
-       {
-           max = Math.max(max, n);
-           
-       }
-      int arr[] = new int[max+1];
-      for ( int i = 0; i < nums.length ; i++)
-      {
-         arr[nums[i]] = arr[nums[i]] + nums[i];
-      }
-    
-      int use = 0;
-      int skip = arr[0];
-      for ( int i = 1; i < arr.length; i++)
-      {
-          int temp = skip;
-          skip = Math.max(use, skip);
-          use = temp + arr[i];
-      }
-        return Math.max(skip, use);
-    }
+    public int deleteAndEarn(int[] nums) {
+        int maxNum = Integer.MIN_VALUE;
+        for ( int num : nums){
+            maxNum = Math.max(maxNum, num);
+        }
+        int[] countsArray = new int[maxNum+1];
+        // fill in the countsArray
+        for (int i = 0; i < nums.length; i++){
+            countsArray[nums[i]] =  countsArray[nums[i]] + nums[i];
+        }
+        int skip = 0;
+        int use = countsArray[0];
+        for ( int i = 1; i < countsArray.length; i++){
+            int temp = skip;
+            skip = Math.max(skip, use);
+            use = temp + countsArray[i];
+        }
+        return Math.max(use, skip);
+    }
 }

cb1291b9e86585416d3a0ce4b9a68ada504a0023
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-18 10:59:50 -0700

    Time: 38 ms (11.40%), Space: 53.6 MB (5.21%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,41 +1,81 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-         if ( coins == null || coins.length == 0)
-            return 0;
-       int[][] dp = new int[coins.length+1][amount+1];
-       
-       // fill first row with 99999
-        for ( int i = 1; i < dp[0].length ; i++)
-        {
-            dp[0][i] = 99999;
-        }
-    
-        for (int i = 1; i < dp.length; i++)
-        {
-            for ( int j = 1; j < dp[0].length; j++)
-            {
-                if (j < coins[i-1])
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j] = Math.min(dp[i-1][j], 1+ dp[i][j-coins[i-1]]);
-                }
-            }
-        }
-        int result = dp[dp.length-1][dp[0].length-1];
-        if (result >= 99999)
-            return -1;
-        return result;
-    }
+    public int coinChange(int[] coins, int amount) {
+        if ( coins == null || coins.length == 0)
+               return 0;
+        int[][] dp = new int[coins.length+1][amount+1];
+        Arrays.fill(dp[0], amount+1);
+        for (int i = 1; i < dp.length; i++){
+            for ( int j = 1; j < dp[0].length; j++){
+                if (j < coins[i-1]  ){
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+        int result = dp[dp.length-1][dp[0].length-1];
+        if (result >= amount+1)
+            return -1;
+        return result;
+    }
 }
-​
-// Recursive solution O(2^N)
 // class Solution {
-//     public int coinChange(int[] coins, int amount) {
-//       if ( coins == null || coins.length == 0)
-//           return -1;
-        
-//          int min = 0;
-//          int result = helper(coins,  0,  amount,  min);
+//     public int coinChange(int[] coins, int amount) {
+//         if (coins == null || coins.length == 0)
+//             return 0;
+//         int[][] dp = new int[coins.length+1][amount+1];
+//         Arrays.fill(dp[0], amount+1);
+//         for (int i = 1; i < dp.length; i++) {
+//             for (int j = 1; j < dp[0].length; j++) {
+//                 // if the amount is less than the coin value, just copy the value from above
+//                 if (j < coins[i-1]){
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//                 else{
+//                    // Min between (1+same row, colum - coin val, upper row val)
+//                     dp[i][j] = Math.min(dp[i-1][j], 1+dp[i][j - coins[i-1]]);
+//                 }
+//             }
+//         }
+//         int result = dp[dp.length-1][dp[0].length-1];
+//         // if combination is impossible return -1
+//         if (result >= amount+1){
+//             return -1;
+//         }
+//         return result;
+//     }
+// }
+//TC: O(2^N)
+//SC: O(N)
+//Did it run successfully on Leetcode?:Yes
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         if ( coins == null || coins.length == 0)
+//             return 0;
+//         return helper(coins, 0, amount, 0);
+//     }
+//     private int helper(int[] coins, int index, int amount, int minNoOfCoins ){
+//         //base
+//         if ( amount == 0){
+//             return minNoOfCoins;
+//         }
+//         if (index >= coins.length || amount < 0){
+//             return -1;
+//         }
+//         //logic
+        
+//         //choose
+//         int case1 = helper(coins, index, amount-coins[index], minNoOfCoins+1);
+        
+//         //dont choose
+//         int case2 = helper(coins, index+1, amount, minNoOfCoins);
+        
+//         if ( case1 == -1){
+//             return case2;
+//         }
+//         if ( case2 == -1){
+//             return case1;
+//         }
+//         return Math.min(case1, case2);           
+//     }
+// }

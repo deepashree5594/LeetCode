dac82674730f878ac6f766e0d009f2953771fc2e
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-18 12:50:45 -0700

    Time: 7 ms (7.12%), Space: 43.7 MB (5.01%) - LeetHub

--- minimum-falling-path-sum/minimum-falling-path-sum.java
+++ minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,30 +1,24 @@
-// TC:O(N*N) -> N*N is the size of matrix A
-// SC:O(1) -> No extra space is required.
-// Did it run successfully on LeetCode? Yes
 class Solution {
-    public int minFallingPathSum(int[][] A) {
-        if (A == null || A.length == 0) 
-            return 0;
-        
-        for ( int i = A.length - 2; i >= 0; i--)
-        {
-            for ( int j = 0; j < A[0].length; j++)
-            {
-                int sum = A[i+1][j];
-                if (j+1 < A[0].length)
-                    sum = Math.min(sum, A[i+1][j+1]);
-                if ( j > 0)
-                    sum = Math.min(sum, A[i+1][j-1]);
-                
-                A[i][j] = A[i][j] + sum;
-            }
-        }
-        
-        int minSum = Integer.MAX_VALUE;
-        for (int n : A[0])
-        {
-            minSum = Math.min(minSum, n);
-        }
-        return minSum;
-}
+    public int minFallingPathSum(int[][] matrix) {
+       if (matrix == null || matrix.length == 0)
+           return 0;
+        for ( int i = matrix.length-2; i>=0; i--){
+            for ( int j = 0; j < matrix[0].length; j++){
+                 int sum = matrix[i+1][j];
+                if ( j > 0){
+                    sum = Math.min(sum, matrix[i+1][j-1]);
+                }
+                if (j+1 < matrix[0].length){
+                    sum = Math.min(sum, matrix[i+1][j+1]);    
+                }
+                 matrix[i][j] = matrix[i][j] + sum;
+            }
+        }
+        // return the minimum in the first row
+        int minSum = Integer.MAX_VALUE;
+        for (int n : matrix[0]){
+            minSum = Math.min(minSum, n);
+        }
+        return minSum;
+    }
 }

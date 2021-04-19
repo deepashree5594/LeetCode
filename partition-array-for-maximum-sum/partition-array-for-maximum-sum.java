f3bc08fe4d349a561495ac2fdbba14efb4a0ad6c
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-18 17:45:21 -0700

    Time: 8 ms (22.47%), Space: 39.8 MB (6.17%) - LeetHub

--- partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,27 +1,20 @@
-// TC: O(N*K)
-// SC: O(N)
-// Did it run successfully On Leetcode? : Yes
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         if ( arr == null || arr.length == 0)
             return 0;
-        int[] dp = new int[arr.length];
-        for ( int i = 0; i < arr.length; i++)
-        {
+        int dp[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
             int max = arr[i];
-            for ( int j = 1; j <= k && i-j+1 >=0 ; j++) 
-            {
-                max = Math.max(max, arr[i- j + 1]);
-                if (i-j >= 0 )
-                {
-                    dp[i] = Math.max(dp[i], dp[i-j] + max * j);
-                }
-                else
-                {
-                     dp[i] = Math.max(dp[i], max * j);
-                }     
+            for (int j = 1; j <= k && i-j+1 >= 0; j++){
+                //incoming element = arr[i-j+1]
+                max = Math.max(max, arr[i-j+1]);
+                if (i-j >= 0){
+                     dp[i] = Math.max(dp[i], dp[i-j] + max * j);
+                }else {
+                    dp[i] = Math.max(dp[i], max * j);
+                }              
             }
         }
-        return dp[arr.length-1];
+        return dp[dp.length-1];
     }
 }

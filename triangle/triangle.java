dadd496e960d4d114f6244f3da26c066597082d9
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-19 19:01:43 -0700

    Time: 1 ms (95.90%), Space: 39.1 MB (48.08%) - LeetHub

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -1,45 +1,16 @@
-// // DP bottom-up
-// //TC: O(N)
-// //SC: O(N)
-// //Did it run successfully on Leetcode? : Yes
-// class Solution {
-//     public int minimumTotal(List<List<Integer>> triangle) {
-//         if (triangle == null || triangle.size() == 0)
-//             return 0;
-//         int[] dp = new int[triangle.size()];
-//         dp[0] = triangle.get(0).get(0);
-        
-//         // DP top-down
-//         for ( int i = dp.length - 2; i >=0; i-- ) {
-//             for ( int j = 0; j <= i; j++) {
-//                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
-//          }
-//        }
-//        return dp[0];
-//     }
-// }
-
-// DP top-down
-//TC: O(N)
-//SC: O(N)
-//Did it run successfully on Leetcode? : Yes
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         if (triangle == null || triangle.size() == 0)
             return 0;
-
-        int[] dp = new int[triangle.size()];
-        
-        //populating last row
-        for (int j = 0; j < dp.length; j++)
-            dp[j] = triangle.get(dp.length-1).get(j);
-        
-        // DP bottom up- start from second-last row
-         for ( int i = dp.length - 2; i >= 0; i-- ) {
-            for ( int j = 0; j <= i; j++) {
-               dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
-         }
-       }
-       return dp[0];
+        int m = triangle.size();
+        int n = triangle.get(m-1).size();
+        List<Integer> lastRow = triangle.get(m-1);
+        int[] dp = new int[m+1];
+        for ( int i = m-1; i>=0; i--){
+         for (int j = 0; j <= i; j++){
+             dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+          }
+        }
+      return dp[0];
     }
 }

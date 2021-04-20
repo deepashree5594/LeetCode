a8dcff02a84ae017fedaf9b53886816c97c9bb0a
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-19 19:19:02 -0700

    Time: 0 ms (100.00%), Space: 36.8 MB (38.25%) - LeetHub

--- arithmetic-slices/arithmetic-slices.java
+++ arithmetic-slices/arithmetic-slices.java
@@ -1,42 +1,17 @@
-// DP
-// TC: O(N)
-// SC: O(N)
-// Did it run successfully on Leetcode? : Yes
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         if (nums == null || nums.length == 0)
             return 0;
+        int prev = 0;
         int result = 0;
-        int[] dp = new int[nums.length];
-        for (int i = 2; i < dp.length ; i++){
-            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2] ){
-                dp[i] = 1 + dp[i-1];
-                result = result + dp[i];
+        for ( int i = 2; i < nums.length; i++){
+            if (nums[i-1] - nums[i] == nums[i-2] - nums[i-1]){
+                prev = prev+1;
+                result = result + prev;
+            }else{
+                prev = 0;
             }
         }
         return result;
     }
 }
-// Bruteforce
-// TC: O(N*N)
-// SC: O(1)
-// Did it run successfully on Leetcode? : Yes
-// class Solution {
-//     public int numberOfArithmeticSlices(int[] nums) {
-//         if (nums == null || nums.length == 0)
-//             return 0;
-//         int result = 0;
-//         for ( int i = 0; i < nums.length - 1; i++)
-//         {
-//             int difference = nums[i+1] - nums[i];
-//             for ( int j = i+1; j < nums.length - 1; j++)
-//             {
-//                 if (nums[j+1] - nums[j] == difference)
-//                     result++;
-//                 else
-//                     break;
-//             }
-//         }
-//         return result;
-//     }
-// }

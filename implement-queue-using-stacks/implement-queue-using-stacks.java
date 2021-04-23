2b78138c23091d5d12b0f726ecebd25842501417
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-23 13:52:31 -0700

    Time: 0 ms (100.00%), Space: 36.9 MB (31.53%) - LeetHub

--- implement-queue-using-stacks/implement-queue-using-stacks.java
+++ implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,60 +1,45 @@
 class MyQueue {
-​
-    /** Initialize your data structure here. */
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
-    
-    public MyQueue() {
-        stack1 = new Stack<Integer>();
-        stack2 = new Stack<Integer>();
-    }
-    
-    /** Push element x to the back of queue. */
-    public void push(int x) {
-        stack1.push(x);
-    }
-    
-    /** Removes the element from in front of queue and returns that element. */
-    public int pop() {
-        
-        if (stack1.isEmpty()) 
-            return -1;  
-        
-        while(!stack1.isEmpty())
-        {
-            stack2.push(stack1.pop());
-        }
-        int poppedElement = stack2.pop();
-        while (!stack2.isEmpty())
-        {
-            stack1.push(stack2.pop());
-        }
-        return poppedElement;
-    }
-    
-    /** Get the front element. */
-    public int peek() {
-        if (stack1.isEmpty()) 
-            return -1;  
-        
-        while(!stack1.isEmpty())
-        {
-            stack2.push(stack1.pop());
-        }
-        int topElement = stack2.peek();
-        while (!stack2.isEmpty())
-        {
-            stack1.push(stack2.pop());
-        }
-        return topElement;
-    }
-    
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        return stack1.isEmpty();
-    }
+    /** Initialize your data structure here. */
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    int peekedElement;
+    public MyQueue() {
+        stack1 = new Stack();
+        stack2 = new Stack();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if (stack1.isEmpty()){
+            peekedElement = x;
+        }
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+          int poppedElement = stack2.pop();
+        if (!stack2.isEmpty())
+          peekedElement = stack2.peek();
+        return poppedElement;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return peekedElement;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return (stack1.isEmpty() && stack2.isEmpty());
+    }
 }
-​
+
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();

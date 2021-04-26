8df581a9bed039fb47a3aaad332a2ce23da4c726
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-26 13:33:51 -0700

    Time: 3 ms (75.06%), Space: 52.9 MB (17.49%) - LeetHub

--- container-with-most-water/container-with-most-water.java
+++ container-with-most-water/container-with-most-water.java
@@ -1,23 +1,18 @@
-// TC: O(N) - N is the length of height array
-// SC: O(1) - no extra space is required
-// Did it run successfully on Leetcode? : Yes
 class Solution {
-    public int maxArea(int[] height) {
-        int maxArea = 0;
-        int i = 0;
-        int j = height.length-1;
-        while ( i < j)
-        {            
-            maxArea = Math.max(maxArea,Math.min(height[i], height[j])* (j-i));
-            if ( height[i] <= height[j])
-            {
-                i++;
-            }
-            else
-            {
-                j--;
-            }                       
-        }
-        return maxArea;
-    }
+    public int maxArea(int[] height) {
+        if ( height == null || height.length == 0)
+            return 0;
+        int result = 0;
+        int i = 0;
+        int j = height.length-1;
+        while ( i < j){
+            result = Math.max(result, Math.min(height[i], height[j]) * (j-i));
+            if (height[i] < height[j]){
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return result;
+    }
 }

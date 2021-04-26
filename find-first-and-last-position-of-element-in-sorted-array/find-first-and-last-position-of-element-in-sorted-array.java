498bb28e260a155c297bd55de32ea39c86dbf221
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-25 18:19:27 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (19.67%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,68 +1,53 @@
-// Time Complexity : Binary Search - O(logN)
-// Space Complexity : O(1)
-// Did this code successfully run on Leetcode : YES
-​
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        if ( nums == null || nums.length == 0) return new int[]{-1, -1};
-  
-        int first = binarySearchFirst(nums, target);
-        int second = binarySearchSecond(nums, target);
-        return new int[]{first, second};
-      }
-​
-      private int binarySearchFirst(int[] nums, int target)
-      {
-          int low = 0;
-          int high = nums.length-1;
-          while ( low <= high)
-          {
-              int mid = low + (high-low)/2;
-              if (nums[mid] == target)
-              {
-                  if (mid == 0 || nums[mid] > nums[mid-1])
-                      return mid;
-                  else
-                      high = mid -1;
-              }
-              else if (nums[mid] > target)
-              {
-                  high = mid - 1;
-              }
-              else
-              {
-                  low = mid + 1;
-              }
-          }
-         return -1;
-      }
-    
-      private int binarySearchSecond(int[] nums, int target)
-      {
-          int low = 0;
-          int high = nums.length-1;
-          while ( low <= high)
-          {
-              int mid = low + (high-low)/2;
-              if (nums[mid] == target)
-              {
-                  if (mid == nums.length-1 || nums[mid+1] > nums[mid])
-                      return mid;
-                  else
-                      low = mid + 1;
-              }
-              else if (nums[mid] > target)
-              {
-                  high = mid - 1;
-              }
-              else
-              {
-                  low = mid + 1;
-              }
-          }
-         return -1;
-      }
+    public int[] searchRange(int[] nums, int target) {
+        if ( nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int[] res = new int[2];
+        res[0] = firstBinarySearch(nums, target);
+        res[1] = secondBinarySearch(nums, target);
+        return res;
+    }
+    private int firstBinarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while ( low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target){
+                if(mid == 0 || nums[mid-1] < nums[mid]){
+                    return mid;
+                } else if (nums[mid-1] == nums[mid]){
+                    high = mid - 1;
+                } else {
+                    low = mid +1;
+                }
+            } else if (nums[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1 ;
+            }
+        }
+        return -1;
+    }
+    private int secondBinarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while ( low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target){
+                if(mid == nums.length-1 || nums[mid] < nums[mid+1]){
+                    return mid;
+                }  else if (nums[mid] == nums[mid + 1]){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else if (nums[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1 ;
+            }
+        }
+        return -1;
+    }
 }
-// Time Complexity : Linear Search - O(2N) - O(N)
-// Space Complexity : O(1)
-// Did this code successfully run on Leetcode : YES

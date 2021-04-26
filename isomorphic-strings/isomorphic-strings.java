2a34e6b21ed9cbccbbf7784488ad924ae47ebb9c
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-25 19:12:06 -0700

    Time: 6 ms (72.60%), Space: 39 MB (50.27%) - LeetHub

--- isomorphic-strings/isomorphic-strings.java
+++ isomorphic-strings/isomorphic-strings.java
@@ -1,56 +1,53 @@
-// Time Complexity: O(N) - length of s and t strings
-// Space Complexity: O(K) - K is constant (128)
-// Did it run sucessfully on Leetcode? Yes
 class Solution {
-//     public boolean isIsomorphic(String s, String t) {
-​
-      
-        
-//      int[] arr1 = new int[128];
-//      int[] arr2 = new int[128];
-        
-//      for (int i = 0; i < s.length(); i++)
-//      {
-//          if (arr1[s.charAt(i) ] != arr2[t.charAt(i) ] )
-//              return false;
-//          arr1[s.charAt(i) ] = i+1;
-//          arr2[t.charAt(i) ] = i+1;
-//      }
-//         return true;
-    
-//     }
-    public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        HashMap<Character, Character> sMap = new HashMap<Character, Character>();
-        HashMap<Character, Character> tMap = new HashMap<Character, Character>();
-        int i = 0; 
-        int j = 0;
-        while ( i < s.length() && j < t.length())
-        {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(j);
-            if (sMap.containsKey(sChar))
-            {
-                if (sMap.get(sChar) != tChar)
-                    return false;
-            }
-            else
-            {
-                sMap.put(sChar, tChar);
-            }
-            if (tMap.containsKey(tChar))
-            {
-                if (tMap.get(tChar) != sChar)
-                    return false;
-            }
-            else
-            {
-                tMap.put(tChar, sChar);
-            }
-            i++;
-            j++;
-        }
-        return true;
-    }
+//     public boolean isIsomorphic(String s, String t) {
+        
+//     }
+//}
+public boolean isIsomorphic(String s, String t) {
+
+       if(s.length() != t.length())    return false;
+
+       HashMap<Character, Character> map = new HashMap<>();
+
+       HashSet<Character> set = new HashSet<>();
+
+       for(int i=0; i<s.length(); i++)
+
+       {
+
+           char c = s.charAt(i);
+
+           char d = t.charAt(i);
+
+           if(!map.containsKey(c))
+
+           {
+
+               if (set.contains(d))
+
+                   return false;
+
+               map.put(c, d);
+
+               set.add(d);
+
+           }
+
+           else
+
+           {
+
+               if(map.get(c) != d)
+
+                   {
+                   //set.add(d);
+
+                   return false;}
+
+           }
+
+       }
+    return true;
+
+}
 }

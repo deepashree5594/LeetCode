4ae3bbdcda565729e41cd48ee8f22a6534e59dbd
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-25 23:06:08 -0700

    Time: 1 ms (100.00%), Space: 50 MB (34.40%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,75 +1,15 @@
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int[] leftarray = new int[nums.length];
-//         int[] rightarray = new int[nums.length];
-//         int[] result = new int[nums.length];
-        
-//         leftarray[0] = 1;
-//         for ( int i = 1; i < leftarray.length; i++)
-//         {
-//             leftarray[i] = leftarray[i-1] * nums[i-1];
-//         }
-        
-//         rightarray[rightarray.length-1] = 1;
-//         for ( int i = rightarray.length-2; i >=0; i--)
-//         {
-//             rightarray[i] = rightarray[i+1] * nums[i+1];
-//         }
-        
-//         for ( int i = 0; i < result.length; i++)
-//         {
-//             result[i] = leftarray[i] * rightarray[i];
-//         }
-        
-//         return result;
-//     }
-// }
-
-
-//https://www.youtube.com/watch?v=tSRFtR3pv74&ab_channel=NickWhite
-
-// TC: O(2N) = O(N) -  N is the no. of elements of nums array
-// SC: O(1) - No extra space required
-// Did it run successfully on Leetcode? : Yes
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if ( nums == null || nums.length == 0)
-            return new int[]{};
-        
         int[] result = new int[nums.length];
         result[0] = 1;
-        for ( int i = 1; i < result.length; i++)
-        {
+        for ( int i = 1; i < result.length; i++){
             result[i] = result[i-1] * nums[i-1];
         }
-        int runningProduct = 1;
-        for (int i = result.length-1; i>= 0; i--)
-        {
-            result[i] = result[i] * runningProduct;
-            runningProduct = runningProduct * nums[i];
+        int r = 1;
+        for (int i = result.length-1; i >=0; i--){
+            result[i] = result[i] * r;
+            r = r * nums[i];
         }
         return result;
     }
 }
-        
-        
-        
-//        int[] result = new int[nums.length];
-        
-//        if ( nums == null || nums.length == 0)
-//            return result;
-//         result[0] = 1;
-//         for ( int i = 1; i < nums.length; i++)
-//         {
-//            result[i] = result[i-1] * nums[i-1];
-//         }  
-//         int runningProduct = 1;
-//         for ( int i = nums.length-1; i>=0; i--)
-//         {
-//             result[i] = runningProduct * result[i] ;
-//             runningProduct = runningProduct * nums[i];
-//         }
-//         return result;
-//     }
-    
-// }

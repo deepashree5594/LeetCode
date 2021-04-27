5687706b096ced9a1ea4c7796b8a01d9e938d285
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-27 13:20:36 -0700

    Time: 2 ms (24.66%), Space: 41.7 MB (8.33%) - LeetHub

---
+++ path-sum-ii/path-sum-ii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        result = new ArrayList();
+        helper(root, targetSum, new ArrayList(), 0);
+        return result;
+    }
+    private void helper(TreeNode root, int targetSum, List<Integer> path, int currSum){
+        //base
+        if ( root == null)
+            return;
+        
+        //logic
+        path.add(root.val);
+        currSum = currSum + root.val;
+        if (root.left == null && root.right == null && currSum == targetSum)
+            result.add(new ArrayList<>(path));
+        helper(root.left, targetSum, new ArrayList<>(path), currSum);
+        helper(root.right, targetSum, new ArrayList<>(path), currSum);
+    }
+}

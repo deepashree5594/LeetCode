cd7996b360738cef44cd23f386ae96c3986b82ed
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-30 11:25:52 -0700

    Time: 0 ms (100.00%), Space: 36.9 MB (97.91%) - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,46 +1,19 @@
-//TC: O(logN)
-//SC: O(1)
 class Solution {
     public double myPow(double x, int n) {
-       //base
-        if ( n == 0) 
+        //base
+        if ( n == 0)
             return 1;
-       //logic
+        //logic
         double temp = myPow(x, n/2);
         //stack.pop() happens
-        if ( n % 2 == 0)      //n is even 
-        {
+        if (n % 2 == 0){
             return temp * temp;
-        }
-        else //n is odd 
-        {
-            if ( n < 0 ) // if n is negative,  multiply it by 1/x
-            {
+        } else {
+            if ( n < 0){
                 return temp * temp * (1/x);
-            }
-            else // if n is positive, just multiply it by x
-            {
+            } else {
                 return temp * temp * x;
             }
         }
     }
 }
-
-// TC: O(N)
-//SC: O(1)
-// class Solution {
-//     public double myPow(double x, int n) {
-//         long N = n;
-//         if ( n < 0)
-//         {
-//             x = 1/x;
-//             N = -N;
-//         }
-//         double ans = 1;
-//         for ( int i = 1; i <= N; i++)
-//         {          
-//               ans = ans * x;         
-//         }
-//         return ans;
-//     }
-// }

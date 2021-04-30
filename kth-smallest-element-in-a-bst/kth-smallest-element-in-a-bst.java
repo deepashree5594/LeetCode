a3a81a66505aeb60b340b0bf545f7c053e5b6a10
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-30 16:54:07 -0700

    Time: 0 ms (100.00%), Space: 38.6 MB (74.05%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,52 +1,36 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode(int x) { val = x; }
-//  * }
-//  */
-// class Solution {
-//     public int kthSmallest(TreeNode root, int k) {
-//         Stack<TreeNode> stack = new Stack<TreeNode>();
-//         while ( !stack.isEmpty() || root != null)
-//         {
-//             while ( root != null)
-//             {
-//                 stack.push(root);
-//                 root = root.left;
-//             }
-//             root = stack.pop();
-//             if ( --k == 0) return root.val;
-//             root = root.right;
-//         }
-//         return -1;
-//     }
-// }
-
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int count;
-    int result;
     public int kthSmallest(TreeNode root, int k) {
-        count = 0;
-        result = 0;
-        inorder(root, k);
-        return result;
-    }
-    private void inorder(TreeNode root, int k)
-    {
-        //base
-        if (root == null)
-          return;
-        //logic
-        inorder(root.left, k);
-        count++;
-        if ( count == k)    
-        { 
-            result = root.val;
+        Stack<TreeNode> stack = new Stack();
+        int result = 0;
+        int count = 0;
+        while (!stack.isEmpty() || root != null){
+            while ( root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            count++;
+            if (count == k){
+                result = root.val;
+                break;
+            }   
+            root = root.right;
         }
-        inorder(root.right, k);
+        return result;
     }
 }

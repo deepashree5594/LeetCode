8a6b41db8a7cabc48ddc99853b224a3b1ced73c7
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-30 16:27:55 -0700

    Time: 18 ms (41.49%), Space: 40.3 MB (25.01%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -9,32 +9,32 @@ class Solution {
         {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        for ( int i = 0; i < s.length(); i++)
-        {
-               char incomingChar = s.charAt(i);
-               if (map.containsKey(incomingChar))
-               {
-                  int count = map.get(incomingChar);
-                  count--;
-                  if (count == 0)
-                       match++;
-                  map.put(incomingChar, count);
-               }
-               if ( i - p.length() + 1 > 0)
-               {
-                  char outgoingChar = s.charAt(i - p.length());
-                  if (map.containsKey(outgoingChar))
-                  {
-                     int count = map.get(outgoingChar);
-                     count++;
-                     if (count == 1)
-                         match--;
-                     map.put(outgoingChar, count);
-                  }
-               }
-             if ( match == map.size())
-                 result.add(i - p.length() + 1);
+        for ( int i = 0; i < s.length(); i++){
+            //incoming char
+            char incomingChar = s.charAt(i);
+            if (map.containsKey(incomingChar)){
+                int count = map.get(incomingChar);
+                count--;
+                if(count == 0){
+                    match++;
+                }
+                map.put(incomingChar, count);
+            }
+           if (i >= p.length()){
+             char outgoingChar = s.charAt(i-p.length());
+                if (map.containsKey(outgoingChar)){
+                    int count = map.get(outgoingChar);
+                    count++;
+                    if(count == 1){
+                     match--;
+                     }
+                    map.put(outgoingChar, count);
+                    }
            }
-      return result;
+            if ( match == map.size()){
+                result.add( i - p.length() + 1);
+            }
+        }
+        return result;
     }
 }

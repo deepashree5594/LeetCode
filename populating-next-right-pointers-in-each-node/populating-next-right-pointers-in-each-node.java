1f29592da429e6386993e04e275efb00c4fe32de
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-30 19:16:00 -0700

    Time: 2 ms (40.70%), Space: 39.3 MB (26.53%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -20,34 +20,67 @@ class Node {
     }
 };
 */
+// TC: O(N) , N -> no. of nodes of the tree
+// SC: O(N) , size of queue -> no. of nodes of the tree
+// Did it run successfully on Leetcode? :Yes
 
-class Solution {
+ class Solution {
     public Node connect(Node root) {
+          //base
           if ( root == null)
             return null;
-        Queue<Node> q = new LinkedList();
-        q.offer(root);
-        while (!q.isEmpty())
-        {
-            int size = q.size();
-            Node prev = q.poll();
-            if (prev.left != null)
-            {
-                q.offer(prev.left);
-                q.offer(prev.right);
-            }
-            for ( int i = 1; i < size; i++)
-            {    
-               Node curr = q.poll();
-                if (curr.left != null)
-                {
-                  q.offer(curr.left);
-                  q.offer(curr.right);
-                }
-               prev.next = curr;
-               prev = curr;
-            }
-        }
+          dfs(root.left, root.right);
         return root;
     }
-}
+    private void dfs(Node left, Node right){
+        //base
+         if ( left == null && right == null)
+            return;
+        //logic
+        left.next = right;
+        dfs(left.left, left.right);
+        dfs(right.left, right.right);
+        dfs(left.right, right.left);
+         
+    }
+ }
+// TC: O(N) , N -> no. of nodes of the tree
+// SC: O(N) , size of queue -> no. of nodes of the tree
+// Did it run successfully on Leetcode? :Yes
+//  class Solution {
+//     public Node connect(Node root) {
+//           if ( root == null)
+//             return null;
+//         Queue<Node> q = new LinkedList();
+//         q.offer(root);
+//         while (!q.isEmpty())
+//         {
+//             int size = q.size();
+//             Node prev = q.poll();
+//             if (prev.left != null)
+//             {
+//                 q.offer(prev.left);
+//             }
+//             if (prev.right != null)
+//             {
+//                 q.offer(prev.right);
+//             }
+//             for ( int i = 1; i < size; i++)
+//             {    
+//                Node curr = q.poll();
+//                System.out.println("prev "  + prev.val + " curr "  + curr.val);
+//                  if (curr.left != null)
+//                  {
+//                   q.offer(curr.left);
+//                  } 
+//                  if (curr.right != null)
+//                  {      
+//                      q.offer(curr.right);
+//                  }
+//                prev.next = curr;
+//                prev = curr;
+//             }
+//         }
+//         return root;
+//     }
+// }

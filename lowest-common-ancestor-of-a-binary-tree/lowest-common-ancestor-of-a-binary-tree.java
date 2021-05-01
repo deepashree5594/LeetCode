80b380023f506d78e566123419f37f341f7d784e
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-04-30 20:50:56 -0700

    Time: 4 ms (100.00%), Space: 40.9 MB (73.01%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -7,49 +7,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-
-
-
-//https://www.youtube.com/watch?v=13m9ZCB8gjw
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {        
-       List<TreeNode> path1 = new ArrayList();
-       List<TreeNode> path2 = new ArrayList();
-       backtrack(root, p, path1);
-       backtrack(root, q, path2);
-     
-       for (int i = 0; i < path1.size(); i++)
-       {
-           if (path1.get(i) != path2.get(i))
-               return path1.get(i-1);
-       }
-       return null;
-    }
-    private void backtrack(TreeNode root, TreeNode target, List<TreeNode> path)
-    {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         //base
-        if ( root == null )
-            return;
-        if (root == target)
-        {
-            path.add(root);
-            path.add(root);
-            return;
-        }
-            
+        if ( root == null || root == p || root == q)
+            return root;
         //logic
-        //action
-        path.add(root);
-        
-        //recurse
-        backtrack(root.left, target, path);
-        backtrack(root.right, target, path);
-        
-        if (path.get(path.size() - 1) == target)
-                return;
-        
-        //backtrack
-        path.remove(path.size()-1);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if ( left == null && right == null){
+            return null;
+        } else if ( left != null && right != null){
+            return root;
+        } else {
+            return (left != null)? left : right;
+        }
     }
 }

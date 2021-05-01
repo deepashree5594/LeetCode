b3a1e8bafc2e44819d2d2fc498a04cd275535a8c
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-01 00:03:08 -0700

    Time: 3 ms (78.86%), Space: 47 MB (27.65%) - LeetHub

--- find-the-town-judge/find-the-town-judge.java
+++ find-the-town-judge/find-the-town-judge.java
@@ -1,22 +1,18 @@
-// TC:
-// SC: O(N) -> O(2N) 
+//TC:O(E)
+//SC:O(V)
 class Solution {
     public int findJudge(int N, int[][] trust) {
         if (trust.length < N-1)
             return -1;
-        
         int[] trustScores = new int[N+1];
-        for (int[] relation : trust)
-        {
+        for (int[] relation : trust){
             //outdegree
             trustScores[relation[0]]--;
+                
             //indegree
             trustScores[relation[1]]++;
         }
-        
-        for (int i = 1; i <= N; i++)
-        {
-            // if indegree-outdegree == N-1, he is the town judge
+        for (int i = 1; i <= N; i++){
             if (trustScores[i] == N-1)
                 return i;
         }

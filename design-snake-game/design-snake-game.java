00f4c9719a93ffc627e4d1c2e4e72ba0b6f47094
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-01 21:27:35 -0700

    Time: 100 ms (7.26%), Space: 109.4 MB (12.37%) - LeetHub

--- design-snake-game/design-snake-game.java
+++ design-snake-game/design-snake-game.java
@@ -1,22 +1,21 @@
 class SnakeGame {
-
+     LinkedList<int[]> snake;
+     LinkedList<int[]> foodList; 
+     int height;
+     int width;
+     int[] snakeHead;
     /** Initialize your data structure here.
         @param width - screen width
         @param height - screen height 
         @param food - A list of food positions
         E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
-    LinkedList<int[]> snake;
-    LinkedList<int[]> foodList;
-    int height;
-    int width;
-    int[] snakeHead;
     public SnakeGame(int width, int height, int[][] food) {
-        this.height = height;
+         this.height = height;
         this.width = width;
+        foodList = new LinkedList(Arrays.asList(food));
         snake = new LinkedList();
-        foodList = new LinkedList<>(Arrays.asList(food));
         snakeHead = new int[]{0,0};
-        snake.add(snakeHead);   //[0,0];
+        snake.add(snakeHead);
     }
     
     /** Moves the snake.
@@ -24,32 +23,23 @@ class SnakeGame {
         @return The game's score after the move. Return -1 if game over. 
         Game over when snake crosses the screen boundary or bites its body. */
     public int move(String direction) {
-        if (direction.equals("U"))
-        {
+        if (direction.equals("U")){
             snakeHead[0]--;
+        } else if (direction.equals("D")){
+            snakeHead[0]++;
+        } else if (direction.equals("L")){
+            snakeHead[1]--;
+        } else if(direction.equals("R")){
+            snakeHead[1]++;
         }
-        if (direction.equals("D"))
-        {
-             snakeHead[0]++;
-        }
-        if (direction.equals("L"))
-        {
-             snakeHead[1]--;
-        }
-        if (direction.equals("R"))
-        {
-             snakeHead[1]++;
-        }
-        // check if snake touches the borders
-        if ( snakeHead[0] < 0 || snakeHead[0] >= height || snakeHead[1] < 0 || snakeHead[1] >= width)
+        if (snakeHead[0] < 0 || snakeHead[0] >= height || snakeHead[1] < 0 || snakeHead[1] >= width){
             return -1;
-        for ( int i = 1; i < snake.size(); i++ )  // start from 1 because 0 would have already moved 
-        {
+        }
+        for (int i = 1; i < snake.size(); i++){
             int[] curr = snake.get(i);
             if (snakeHead[0] == curr[0] && snakeHead[1] == curr[1])
                 return -1;
         }
-        // when snake eats food
         if (!foodList.isEmpty())
         {
             int[] foodPixel = foodList.peek();
@@ -59,10 +49,9 @@ class SnakeGame {
                 return snake.size() - 1 ;
             }
         }
-        // when snake moves
-        snake.remove();   //remove the tail of the snake. It is basuically head of our list ( because when snake moves, tail of snake moves)
+        snake.remove();
         snake.add(new int[]{snakeHead[0],snakeHead[1]});
-        return snake.size() - 1 ;
+        return snake.size()-1;
         
     }
 }

9e4fb4b421d5d67a9721583c7238f33177bf4d76
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-02 10:39:09 -0700

    Time: 4 ms (97.16%), Space: 39 MB (37.39%) - LeetHub

--- peeking-iterator/peeking-iterator.java
+++ peeking-iterator/peeking-iterator.java
@@ -2,43 +2,39 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
-    Iterator<Integer> itr;
-    Integer peekedValue = null;
+    Iterator<Integer> it;
+   Integer peekedValue;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-	    this.itr = iterator;
+	    peekedValue = null;
+        this.it = iterator;
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        if (peekedValue == null)
-        {
-          if (itr.hasNext()){
-             peekedValue = itr.next();
-            }
-	     }
+       if (peekedValue == null){
+           peekedValue = it.next();
+       }
         return peekedValue;
-    }
+	}
+	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-            if (peekedValue != null){
-                System.out.println(" next() peekedValue = " + peekedValue );
-                Integer returnValue = peekedValue;
-                peekedValue = null;
-                return returnValue;
-            }
-            else {
-                  return itr.next();      
-            }
-        }
+	    if (peekedValue != null){     
+            Integer returnValue = peekedValue;
+            peekedValue = null;
+            return returnValue;
+        } else {
+            return it.next();
+        } 
+	}
 	
 	@Override
 	public boolean hasNext() {
-        if (peekedValue != null){
-            return true;
-        }
-	    return itr.hasNext();
+	     if (peekedValue != null)
+             return true;
+         return it.hasNext();
 	}
 }

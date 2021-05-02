7edc4e4a841fd08ae323d03f23a3b23775bd07f9
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-01 18:45:08 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (5.56%) - LeetHub

---
+++ h-index/h-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+        for (int i = 0; i < citations.length; i++){
+            if (citations[i] < n){
+                buckets[citations[i]]++;
+            } else {
+                 buckets[n]++;
+            }
+        }
+        int sum = 0;
+        for (int i = n; i>= 0; i--){
+           sum = sum + buckets[i];
+           if (i <= sum){
+               return i;
+           }
+        }
+        return -1;
+    }
+}

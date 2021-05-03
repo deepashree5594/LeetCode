5d3a15593a4bdc257cf7cfcd33a25e8451265674
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-03 16:26:37 -0700

    Time: 8 ms (74.18%), Space: 39.4 MB (44.27%) - LeetHub

--- super-egg-drop/super-egg-drop.java
+++ super-egg-drop/super-egg-drop.java
@@ -1,21 +1,38 @@
 // //TC: O(N*K) -> N: no.of floors, K: no.of eggs
 // //SC: O(N*K)  -> N: no.of floors
 // // Did it run successfully on Leetcode? : TLE
+// class Solution {
+//     public int superEggDrop(int k, int n) {
+//         int attempts = 0;
+//         int[][] dp = new int[n+1][k+1];
+//         while(dp[attempts][k] < n) {   
+//              attempts++;
+//             for(int j = 1; j <= k; j++) {
+//                 dp[attempts][j] = 1 + dp[attempts-1][j-1] + dp[attempts-1][j];
+//             } 
+//         }
+//         return attempts;
+//     }
+// }
+
+// //TC: O(N*K) -> N: no.of floors, K: no.of eggs
+// //SC: O(N*K)  -> N: no.of floors
+// // Did it run successfully on Leetcode? : TLE
 class Solution {
     public int superEggDrop(int k, int n) {
         int attempts = 0;
         int[][] dp = new int[n+1][k+1];
-        while(dp[attempts][k] < n) {   
-             attempts++;
-            for(int j = 1; j <= k; j++) {
-                dp[attempts][j] = 1 + dp[attempts-1][j-1] + dp[attempts-1][j];
-            } 
+        for (int i = 1; i < dp.length; i++){
+            for (int j = 1; j < dp[0].length; j++){
+                dp[i][j] = 1 + dp[i-1][j-1] + dp[i-1][j];
+                if (dp[i][j] >= n){
+                    return i;
+                }
+            }
         }
-        return attempts;
+        return n;
     }
 }
-
-
 
 
 //TC: O(N*K*N) -> N: no.of floors, K: no.of eggs

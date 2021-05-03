ea840f498dcc3de765cc544028b96b5399595864
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-03 08:53:06 -0700

    Time: 0 ms (100.00%), Space: 37 MB (27.09%) - LeetHub

--- arithmetic-slices/arithmetic-slices.java
+++ arithmetic-slices/arithmetic-slices.java
@@ -1,6 +1,9 @@
+//TC:O(N)
+//SC:O(1)
+//Did it run successfully on Leetcode?:Yes
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        if (nums == null || nums.length == 0)
+        if (nums == null || nums.length < 2)
             return 0;
         int prev = 0;
         int result = 0;

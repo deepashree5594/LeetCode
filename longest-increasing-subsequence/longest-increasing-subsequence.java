b3892edb8bd13288a5e39f125691d9b1b9aff0b9
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-03 09:34:24 -0700

    Time: 2 ms (98.99%), Space: 38.6 MB (44.30%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,32 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int maxLen = 1;
-        if(nums == null || nums.length == 0)
+        if (nums == null || nums.length == 0)
             return 0;
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        for (int i = 0; i < nums.length ;i++){
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    maxLen = Math.max(maxLen, dp[i]);
-                }
+        int n = nums.length;
+        int[] effectiveIncreasingArray = new int[n];
+        int len = 1;
+        effectiveIncreasingArray[0] = nums[0];
+        for (int i = 1; i < n; i++){
+            if (nums[i] > effectiveIncreasingArray[len-1]) {
+                effectiveIncreasingArray[len] = nums[i];
+                len++;
+            } else {
+                int index = closesBinarySearch(0, len, effectiveIncreasingArray, nums[i]);
+                effectiveIncreasingArray[index] = nums[i];   
             }
         }
-        return maxLen;
+        return len;
+    }
+    private int closesBinarySearch(int low, int high, int[] arr, int target){
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target){
+                 low = mid+1;
+            } else 
+                high = mid-1;
+        }
+        return low;
     }
 }

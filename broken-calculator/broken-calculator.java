92c0d1f7471b0ef5f1d01a6947178864112dbc18
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-04 15:27:46 -0700

    Time: 0 ms (100.00%), Space: 35.8 MB (42.95%) - LeetHub

--- broken-calculator/broken-calculator.java
+++ broken-calculator/broken-calculator.java
@@ -1,5 +1,11 @@
+//Greedy
+//TC: O(logY)
+//SC: O(1)
+//Did it run successfully on Leetcode?:Yes
 class Solution {
     public int brokenCalc(int X, int Y) {
+        if (X == Y)
+          return 0;
         int count = 0;
         while ( Y > X ){
             if (Y % 2  == 0){
@@ -10,7 +16,38 @@ class Solution {
             }
             count++;
         }
-        count = count + X - Y;
+        count = count + Math.abs(Y - X);
         return count;
     }
 }
+
+//BFS
+//TC: O(exponential)
+//SC: O(N) - N is very large, for every intermediate number, we add 2 possible values
+//Did it run successfully on Leetcode?:TLE
+// class Solution {
+//     public int brokenCalc(int X, int Y) {
+//         if (X == Y)
+//             return 0;
+//         Queue<Integer> q = new LinkedList();
+//         q.add(X);
+//         int steps = 0;
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for (int i = 0; i < size; i++){
+//                 int curr = q.poll();
+//                 if ( curr == Y)
+//                     return steps;
+//                 if (curr > Y) {
+//                     q.offer(curr-1);
+//                 } 
+//                 else{
+//                      q.offer(curr-1);
+//                      q.offer(curr * 2);
+//                 } 
+//             }
+//             steps++;
+//         }
+//         return Integer.MAX_VALUE;
+//     }
+// }

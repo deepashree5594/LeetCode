edca5c8e6fd39ef1cd246a958eade15e21a34e05
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2021-05-05 20:35:42 -0700

    Time: 1 ms (97.19%), Space: 36.7 MB (86.39%) - LeetHub

---
+++ guess-the-word/guess-the-word.java
@@ -0,0 +1,28 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Master {
+ *     public int guess(String word) {}
+ * }
+ */
+class Solution {
+    public void findSecretWord(String[] wordlist, Master master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            String guess = wordlist[new Random().nextInt(wordlist.length)];
+            x = master.guess(guess);
+            List<String> wordlist2 = new ArrayList<>();
+            for (String w : wordlist)
+                if (match(guess, w) == x)
+                    wordlist2.add(w);
+            wordlist = wordlist2.toArray(new String[wordlist2.size()]);
+        }
+    }
+      public int match(String a, String b) {
+        int matches = 0;
+        for (int i = 0; i < a.length(); ++i)
+            if (a.charAt(i) == b.charAt(i))
+                matches ++;
+        return matches;
+    }
+}
+   
